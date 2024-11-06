4b2b8caadc13f560eb5b9b6c3f0298164875e39c
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-11-05 21:01:35 -0500

    updated root folder structure

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -8,3 +8,9 @@
 // THEN I receive a notification if I have entered an invalid email address
 // WHEN I am presented with the Resume section
 // THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import {Modal, Tooltip, Collapse, Dropdown, Alert, Button} from 'bootstrap'
